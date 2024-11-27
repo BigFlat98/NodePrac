1eb62a453fb4a66ddff203d5c4beaecacc61a346
BigFlat98 <hdj9836@naver.com>
2024-11-27 21:25:30 +0900

    solve cookie problem

--- webSever/cookie/cookieT.js
+++ webSever/cookie/cookieT.js
@@ -2,17 +2,17 @@ const http = require('http');
 const fs = require('fs').promises;
 const path = require('path');
 
-const parseCookies = (cookie = '') => {//쿠키의 데이터를 우리가 사용할 수 있는 형태로 변경하는 코드. 
-    cookie.split(';')//cookie의 정보들은 구분자가 ; 으로 돼있음 {'name=hh;id=ttsk;'} -> ['name=hh','id=ttsk'...]
+const parseCookies = (cookie = '') => //쿠키의 데이터를 우리가 사용할 수 있는 형태로 변경하는 코드. 
+    cookie.split(';')   //cookie의 정보들은 구분자가 ; 으로 돼있음 {'name=hh;id=ttsk;'} -> ['name=hh','id=ttsk'...]
     .map(v => v.split('=')) //[['name','hh'],['id','ttsk']...]
     .reduce((acc, [k,v]) =>{ //reduce() 배열의 각 요소에 대해 주어진 함수를 실행하고, 하나의 결과값을 반환하는 메서드.
         acc[k.trim()] = decodeURIComponent(v); //trim() 공백 제거 , decodeURIComponent() 한글 등 디코딩, 한글 같은게 들어오면 %%DE%TJ.. 이런식으로 나오기 때문에 디코딩 해줌.
         return acc;
     },{});
-}
+
 
 http.createServer(async (req,res) => {
-    const cookies = parseCookies(req.headers.cookie);
+    const cookiee = parseCookies(req.headers.cookie);
     //console.log(req.url, cookies);
     if(req.url.startsWith('/login')){ //login요청이 들어오면 쿠키를 설정하는 코드.
         const url = new URL(req.url, 'http://localhost:8084'); //req.url은 url 전체가 들어오는게 아님 port 뒤에 붙는 부분만 들어옴.
@@ -25,10 +25,9 @@ http.createServer(async (req,res) => {
         });
         res.end();
     }
-    else if(cookies.name){//쿠키에 이름이 있으면 이미 로그인을 했다는 뜻.
-        console.log(cookies.name);
+    else if(cookiee.name){ //쿠키에 이름이 있으면 이미 로그인을 했다는 뜻.
         res.writeHead(200,{'Content-Type':'text/plain; charset=utf-8'});
-        res.end(`<h1>${cookies.name}님 안녕하세요.</h1>`);
+        res.end(`<h1>${cookiee.name}님 안녕하세요.</h1>`);
     }
     else{
         try{
